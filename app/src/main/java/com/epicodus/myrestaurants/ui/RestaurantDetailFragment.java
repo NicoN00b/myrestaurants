54cb9a2b0ec7fd72d322ac9471346d0a4a58db21
nr <nicholas.raethke@gmail.com>
2017-10-19 15:49:57 -0700

    Follow through week two

--- app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/RestaurantDetailFragment.java
@@ -1,6 +1,8 @@
 package com.epicodus.myrestaurants.ui;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -18,17 +20,19 @@ import org.parceler.Parcels;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class RestaurantDetailFragment extends Fragment {
-      @Bind(R.id.restaurantImageView) ImageView mImageLabel;
-      @Bind(R.id.restaurantNameTextView) TextView mNameLabel;
-      @Bind(R.id.cuisineTextView) TextView mCategoriesLabel;
-      @Bind(R.id.ratingTextView) TextView mRatingLabel;
-      @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
-      @Bind(R.id.phoneTextView) TextView mPhoneLabel;
-      @Bind(R.id.addressTextView) TextView mAddressLabel;
-      @Bind(R.id.saveRestaurantButton) TextView mSaveRestaurantButton;
+public class RestaurantDetailFragment extends Fragment implements View.OnClickListener {
+    private static final int MAX_WIDTH = 400;
+    private static final int MAX_HEIGHT = 400;
+    @Bind(R.id.restaurantImageView) ImageView mImageLabel;
+    @Bind(R.id.restaurantNameTextView) TextView mNameLabel;
+    @Bind(R.id.cuisineTextView) TextView mCategoriesLabel;
+    @Bind(R.id.ratingTextView) TextView mRatingLabel;
+    @Bind(R.id.websiteTextView) TextView mWebsiteLabel;
+    @Bind(R.id.phoneTextView) TextView mPhoneLabel;
+    @Bind(R.id.addressTextView) TextView mAddressLabel;
+    @Bind(R.id.saveRestaurantButton) TextView mSaveRestaurantButton;
 
-      private Restaurant mRestaurant;
+    private Restaurant mRestaurant;
 
 
     public static RestaurantDetailFragment newInstance(Restaurant restaurant) {
@@ -51,7 +55,11 @@ public class RestaurantDetailFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_restaurant_detail, container, false);
         ButterKnife.bind(this, view);
 
-        Picasso.with(view.getContext()).load(mRestaurant.getImageUrl()).into(mImageLabel);
+        Picasso.with(view.getContext())
+                .load(mRestaurant.getImageUrl())
+                .resize(MAX_WIDTH, MAX_HEIGHT)
+                .centerCrop()
+                .into(mImageLabel);
 
         mNameLabel.setText(mRestaurant.getName());
         mCategoriesLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getCategories()));
@@ -59,7 +67,31 @@ public class RestaurantDetailFragment extends Fragment {
         mPhoneLabel.setText(mRestaurant.getPhone());
         mAddressLabel.setText(android.text.TextUtils.join(", ", mRestaurant.getAddress()));
 
+        mWebsiteLabel.setOnClickListener(this);
+        mPhoneLabel.setOnClickListener(this);
+        mAddressLabel.setOnClickListener(this);
+
         return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == mWebsiteLabel) {
+            Intent webIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse(mRestaurant.getWebsite()));
+            startActivity(webIntent);
+        }
+        if (v == mPhoneLabel) {
+            Intent phoneIntent = new Intent(Intent.ACTION_DIAL,
+                    Uri.parse("tel:" + mRestaurant.getPhone()));
+            startActivity(phoneIntent);
+        }
+        if (v == mAddressLabel) {
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("geo:" + mRestaurant.getLatitude()
+                            + "," + mRestaurant.getLongitude()
+                            + "?q=(" + mRestaurant.getName() + ")"));
+            startActivity(mapIntent);
+        }
+    }
 }
